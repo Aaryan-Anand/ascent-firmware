0650f3a3d4b8dc36de5b162a746323dd72cfa597
Abdul Zia <abdulzia@asu.edu>
2025-04-18 11:33:03 -0700

    please fix

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+#include "ascent_r2_hardware_definition.h"  // Hardware definitions
+
 void app_main() {
     printf("Hello, World!\n");
 }
