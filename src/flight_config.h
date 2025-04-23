e88fae1c138a592c49c8c3faab5e8a67c4a2d913
Abdul Zia <abdulzia@asu.edu>
2025-04-23 03:26:55 -0700

    battery voltage tx

--- src/flight_config.h
+++ src/flight_config.h
@@ -1,10 +1,12 @@
 #ifndef FLIGHT_CONFIG_H
 #define FLIGHT_CONFIG_H
 
-#define APOGEE_MIN 300
-#define MAINS_ALT 5000
-#define POWERED_ALT 30
+#define APOGEE_MIN 50
+#define MAINS_ALT 125
+#define POWERED_ALT 15
 
 // #define LED_PYRO 1
+
+#define LIVE_VIDEO_PYRO_3 1
 
 #endif
