19073c22a6736becf83c95105cbb9d9599652889
Abdul Zia <abdulzia@asu.edu>
2025-04-23 13:04:37 -0700

    fixed tx

--- src/lora_task.c
+++ src/lora_task.c
@@ -63,7 +63,7 @@ void lora_task()
     memcpy(packet_data, &packet, sizeof(lora_packet_t));
     lora_send_packet(packet_data, sizeof(lora_packet_t));
 
-    printf("Sent packet at %ld ms: Latitude: %.6f, Longitude: %.6f, GPSAltitude: %ld, Baro Altitude: %f, Baro Velocity: %f, Acceleration: %f, Pyro Arm: %d, Flight State: %d, Battery Voltage: %f\n", packet.timestamp, packet.latitude, packet.longitude, packet.gps_altitude, packet.barometric_agl, packet.barometric_velocity, packet.acceleration, packet.pyro_arm, packet.flight_state, packet.batt_voltage);
+    // printf("Sent packet at %ld ms: Latitude: %.6f, Longitude: %.6f, GPSAltitude: %ld, Baro Altitude: %f, Baro Velocity: %f, Acceleration: %f, Pyro Arm: %d, Flight State: %d, Battery Voltage: %f\n", packet.timestamp, packet.latitude, packet.longitude, packet.gps_altitude, packet.barometric_agl, packet.barometric_velocity, packet.acceleration, packet.pyro_arm, packet.flight_state, packet.batt_voltage);
 
     int lost = lora_packet_lost();
     if (lost != 0) {
