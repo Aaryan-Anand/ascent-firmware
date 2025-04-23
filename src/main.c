e117698477c38dfde35878e3fbce1558d0a4ccc1
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-04-23 05:10:05 -0700

    created flightState_manger.c

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+#include "esp_psram.h"
+#include "esp_task_wdt.h"
+#include <inttypes.h>
+
+#include <rom/ets_sys.h>
+
+#include "driver_H3LIS331DL.h"
+#include "interface_bmp390l.h"
+#include "interface_sam_m10q.h"
+#include "lora.h"
+#include "driver_buzzer.h"
+#include "driver_pyro.h"
+#include "driver_psu.h"
+#include "driver_bno055.h"
+#include "driver_w25qxx.h"
+
+#include "esp_wifi.h"
+#include "esp_cpu.h"
+#include "xtensa/core-macros.h"
+
+#include "spi_manager.h"
+
+#include "megolavania.h"
+
+#include "driver/gpio.h"
+#include "neopixel.h"
+
+#define PIXEL_COUNT  1
+#define NEOPIXEL_PIN GPIO_NUM_21
+static tNeopixelContext neopixel;
+
+#include "math.h"
+
+#include "ascent_r2_hardware_definition.h"  // Hardware definitions
+
+#include "flight_config.h"
+#include "lora_task.h"
--- src/sensor_manager.c
+++ src/sensor_manager.c
@@ -12,14 +12,61 @@ void bno_flight_init(){
 
     bno_setoprmode(CONFIG);
     bno_configure_acc(NORMAL, ACC_C_H1000, ACC_C_RANGE_16G);
-    bno_set_acc_hgtresh(187);// ~3g
-    bno_set_acc_hgduration(10);// ~20ms above 3g
-    bno_set_acc_int(true, true, true, false, false, false, 0);// HG on X/Y/Z
+    bno_set_acc_hgtresh(187);//3g
+    bno_set_acc_hgduration(10);//20ms above 3g
+    bno_set_acc_int(true, true, true, false, false, false, 0);//HG on X/Y/Z
     bno_setinterruptenable(false, false, true, false, false, false, false, false);
     bno_setinterruptmask(false, false, true, false, false, false, false, false);
     bno_setoprmode(AMG);
 }
 
 void lis331_flight_init(){
-    
+    h3lis331dl_init(I2C_MASTER_PORT);
+
+    h3lis331dl_set_power_mode(H3LIS331DL_NORMAL);
+    h3lis331dl_set_datarate(H3LIS331DL_DATARATE_1000HZ);
+    h3lis331dl_set_axes_config(H3LIS331DL_CONFIG_XY);//Only X and Y
+    h3lis331dl_set_scale(H3LIS331DL_SCALE_100G);
+    h3lis331dl_set_endian(H3LIS331DL_BIG_ENDIAN);
+
+    //high-side int detection
+    uint8_t enables_mask = (1 << 1) | (1 << 3); //XHIE and YHIE
+    h3lis331dl_set_int_cfg(H3LIS331DL_INT1, enables_mask, false);//OR mode
+
+    //Threshold: 2.828g (Net 4G with gravity -> 3g power acceleration)
+    h3lis331dl_set_int_threshold(H3LIS331DL_INT1, 2.828);
+
+    // Duration: 5 → 5ms @ 1000Hz
+    h3lis331dl_set_int_duration(H3LIS331DL_INT1, 5);
+
+    // Optional cleanup
+    h3lis331dl_set_int_level(H3LIS331DL_INT_ACTIVE_LOW);
+    h3lis331dl_set_int_pin_mode(H3LIS331DL_INT_PUSH_PULL);
+    h3lis331dl_set_int1_latch(true);
+}
+
+
+
+
+
+
+
+
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+// ISR handler
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+// Task to run on interrupt
+static void bno_interrupt_task(void* arg) {
+    uint32_t io_num;
+    while (true) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            ESP_LOGI("BNO", "Interrupt detected on GPIO %d", io_num);
+        }
+    }
 }
