23b44e2feba39e963956444fe3bfad894d22db98
worldwalker2000 <space3334@gmail.com>
2025-05-12 14:34:15 -0700

    it just works no for some reason????

--- src/main.c
+++ src/main.c
@@ -47,6 +47,10 @@
 #include "interface_bno055.h"
 #include "interface_h3lis331dl.h"
 
+#include "lora_task.h"
+#include "datalogging.h"
+#include "spi_manager.h"
+
 //#include "flightState_manager.h"
 
 #define FUNCTION_DURATION
@@ -70,24 +74,50 @@ imu_raw_3d_t acc, gyr, mag;         // BNO055 IMU data
 imu_float_3d_t high_g_acc;          // H3LIS331DL high-G accelerometer data
 baro_double_t baro;                 // BMP390 barometer data
 
+extern uint32_t addr;
+#include <stdlib.h>
+
 void app_main(void) {
-    printf("Initializing sensors...");
-    
-    // Initialize I2C bus (which also initializes the sensor interface mutexes)
-    i2c_init();
-    printf("BNO055 IMU initialized");
-    lis331_flight_init();
-    printf("H3LIS331DL accelerometer initialized");
-    bmp_flight_init();
-    printf("BMP390 barometer initialized");
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    printf("All sensors initialized");
-    printf("Starting sensor reading loop");
+    printf("Hello, World!\n");
+
+    psu_init_default();
+
+    spi_manager_init(SPI2_HOST, PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCK);
+
+    lora_task_init();
+
+    uint8_t res;
+    res = w25qxx_init();
+    if (res) printf("Failed to initalize flash\n");
     
     while (1) {
-        bno055_get_local(&acc, &gyr, &mag, false);
-        h3lis331dl_get_local(&high_g_acc, false);
-        bmp390_get_local(&baro);
-        vTaskDelay(10 / portTICK_PERIOD_MS);  // 10ms = 100Hz update rate
+        printf("Trying to use lora\n");
+        lora_task();
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
+
+        printf("Trying to use flash\n");
+        addr = rand() % 500;
+        save_addr();
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+
+    // printf("Initializing sensors...");
+    
+    // // Initialize I2C bus (which also initializes the sensor interface mutexes)
+    // i2c_init();
+    // printf("BNO055 IMU initialized");
+    // lis331_flight_init();
+    // printf("H3LIS331DL accelerometer initialized");
+    // bmp_flight_init();
+    // printf("BMP390 barometer initialized");
+    // vTaskDelay(100 / portTICK_PERIOD_MS);
+    // printf("All sensors initialized");
+    // printf("Starting sensor reading loop");
+    
+    // while (1) {
+    //     bno055_get_local(&acc, &gyr, &mag, false);
+    //     h3lis331dl_get_local(&high_g_acc, false);
+    //     bmp390_get_local(&baro);
+    //     vTaskDelay(10 / portTICK_PERIOD_MS);  // 10ms = 100Hz update rate
+    // }
 }
