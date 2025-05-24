1208af9f4d355b6ca198148ba903d2437d8ac7a8
worldwalker2000 <space3334@gmail.com>
2025-05-23 23:08:29 -0700

    flight state manager

---
+++ src/flight_config.h
@@ -0,0 +1,13 @@
+#ifndef FLIGHT_CONFIG_H
+#define FLIGHT_CONFIG_H
+
+#include "stdbool.h"
+
+#define IS_TWO_STAGE (false)
+
+#define APOGEE_MIN 500
+#define BOOSTER_MIN 300
+#define MAINS_ALT 60
+#define POWERED_ALT 15
+
+#endif
