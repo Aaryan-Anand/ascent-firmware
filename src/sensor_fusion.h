b92d6f46967c2434ba93eabdd4c63d15aea4a9c3
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-06-11 20:53:12 -0500

    fixed it more

--- src/sensor_fusion.h
+++ src/sensor_fusion.h
@@ -80,10 +80,10 @@ void euler_rates_to_quaternion_derivative(const quat_t* current_quat,
 void quaternion_to_euler(const quat_t* quat, orientation_t* euler);
 
 // Get current orientation in Euler angles (degrees)
-void get_gyr_orientation_euler(orientation_t* euler);
+void get_orientation_euler(orientation_t* euler);
 
 // Set orientation from Euler angles (degrees)
-void set_gyr_orientation_euler(const orientation_t* euler);
+void set_orientation_euler(const orientation_t* euler);
 
 // Update orientation using gyroscope data
 void update_orientation_from_gyro(const imu_local_3d_t* gyr);
