ea9a5d464c196a56e095d3d1b4011a9a829e7b60
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-09-18 05:38:04 -0700

    Added on_pad gyro bias correction

--- src/sensor_manager.h
+++ src/sensor_manager.h
@@ -87,5 +87,6 @@ void accl_update(imu_local_3d_t acc, imu_local_3d_t* out);
 
 void baro_update(const baro_double_t * const baro, float *agl, float *vel, float *avg_vel);
 
+void calibrate_gyr_bias_5s(bool use_filtered);
 
 #endif // SENSOR_MANAGER_H
