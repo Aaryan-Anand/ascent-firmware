c363c719e446953ce38383367d5b0a523aca22f4
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-09-18 02:39:20 -0700

    Added Gyro Orientation

--- src/globals.h
+++ src/globals.h
@@ -4,6 +4,16 @@
 #include "sensor_manager.h"
 #include "stdint.h"
 
+#include "orientation.h"
+#include "freertos/semphr.h"
+
+// Shared orientation across tasks
+extern orientation_t g_orientation;
+extern SemaphoreHandle_t g_orientation_mutex;
+
+// Initialize global objects (e.g., mutexes)
+void globals_init(void);
+
 
 
 #endif
