d150f35c39597921b92cec7d558ca159155b6f57
El1zabethS <elizabeth.seaton35@gmail.com>
2025-10-11 16:31:24 -0700

    all key goober functions shouldish be moved to goober from lora. now need to call goober functions back in lora to complete the interface

--- src/goober.c
+++ src/goober.c
@@ -1,6 +1,37 @@
 #include "goober.h"
-#include "lora_interface.h"
 #include "driver_pyro.h"
+#include "stdatomic.h"
+#include "string.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+
+
+static bool CAMERA_ACTIVE = false;
+
+static bool TXLOCK = false;
+
+_Atomic bool thread_safe_txlock = false;
+_Atomic bool thread_safe_should_wakeup = true;
+goober_t recv_packet;
+
+void turn_on_cameras(void) {
+    pyro_activate(PYRO_CHANNEL_3,0,1); 
+    CAMERA_ACTIVE = true;
+}
+
+void turn_off_cameras(void) {
+    pyro_activate(PYRO_CHANNEL_3,1,1); 
+    CAMERA_ACTIVE = false;
+}
+
+// DO NOT REMOVE, required to allow flight with only one way coms - Luke
+void fake_tx_lock(void) {
+	TXLOCK = flash_prepare_for_flight();
+	bmp_aquire_ground();
+	flash_erase_jingle();
+	atomic_store(&thread_safe_txlock, true);
+	printf("Faked tx lock ready to fly.\n");
+}
 
 goober_t create_packet(uint8_t dev_id, bool is_master, bool tx_intent, bool tx_lock, goober_msg_type_t message_class, uint8_t payload_size, goober_payload_t *payload) {
     goober_t packet;
@@ -34,7 +65,6 @@ goober_t create_packet(uint8_t dev_id, bool is_master, bool tx_intent, bool tx_l
 }
 
 goober_t decode_packet(uint8_t *rx_buffer, uint8_t rx_buffer_size, goober_payload_t telemetry) {
-	goober_t recv_packet;
 
 	recv_packet.DEV_ID = rx_buffer[0];
 	recv_packet.DEV_MODE = rx_buffer[1];
@@ -46,7 +76,119 @@ goober_t decode_packet(uint8_t *rx_buffer, uint8_t rx_buffer_size, goober_payloa
   return recv_packet;
 }
 
-goober_t create_response_packet(goober_msg_type_t request_msg_type) {}
+goober_t create_response_packet(goober_msg_type_t request_msg_type) {
+	goober_msg_type_t request_msg_type = recv_packet.MSG_CLS;
+	goober_payload_t request_payload;
+	memcpy(request_payload.raw, recv_packet.payload.raw, recv_packet.PAYLOAD_SIZE);
+
+	goober_t resp;
+	goober_msg_type_t resp_msg_cls =0;
+	goober_payload_t resp_msg_payload;
+	uint8_t resp_msg_payload_len = 0;
+
+	switch (request_msg_type) {
+		case MSG_TYPE_REQ_TELEM: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_TELEM\n");
+			#endif
+			resp_msg_cls = MSG_TYPE_POST_TELEM;
+			resp_msg_payload = telemetry;
+			resp_msg_payload_len = TELEM_PACKET_SIZE;
+			break;
+		}
+		case MSG_TYPE_REQ_AUX_ACTIVATE: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_AUX_ACTIVATE\n");
+			#endif
+			turn_on_cameras();
+			turn_on_fan();
+			resp_msg_cls = MSG_TYPE_POST_TELEM;
+			resp_msg_payload = telemetry;
+			resp_msg_payload_len = TELEM_PACKET_SIZE;
+			break;
+		}
+		case MSG_TYPE_REQ_AUX_DEACTIVATE: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_AUX_DEACTIVATE\n");
+			#endif
+			turn_off_cameras();
+			turn_off_fan();
+			resp_msg_cls = MSG_TYPE_POST_TELEM;
+			resp_msg_payload = telemetry;
+			resp_msg_payload_len = TELEM_PACKET_SIZE;
+			break;
+		}
+		case MSG_TYPE_REQ_TXLOCK_ACTIVATE: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_TXLOCK_ACTIVATE\n");
+			#endif
+			TXLOCK = flash_prepare_for_flight();
+			bmp_aquire_ground();
+			flash_erase_jingle();
+			resp_msg_cls = POST_TXLOCK_ACTIVATE;
+			resp_msg_payload.single_byte.single_byte_payload = 0x79;
+			resp_msg_payload_len = 1;
+			atomic_store(&thread_safe_txlock, true);
+			break;
+		}
+		case MSG_TYPE_REQ_REBOOT: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_REBOOT\n");
+			#endif
+			esp_restart();
+			break;
+		}
+		case MSG_TYPE_REQ_PINGPONG: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_PINGPONG\n");
+			#endif
+			resp_msg_cls = MSG_TYPE_POST_PINGPONG;
+			resp_msg_payload.single_byte.single_byte_payload = 0x01;
+			resp_msg_payload_len = 1;
+			break;
+		}
+		case MSG_TYPE_REQ_POP_APOGEE: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_POP_APOGEE\n");
+			#endif
+			resp_msg_cls = MSG_TYPE_POST_TELEM;
+			resp_msg_payload = telemetry;
+			resp_msg_payload_len = TELEM_PACKET_SIZE;
+			deploy(APPO);
+			break;
+		}
+		case MSG_TYPE_REQ_POP_MAINS: {
+			#ifdef LORA_DEBUG
+			printf("Received REQ_POP_MAINS\n");
+			#endif
+			resp_msg_cls = MSG_TYPE_POST_TELEM;
+			resp_msg_payload = telemetry;
+			resp_msg_payload_len = TELEM_PACKET_SIZE;
+			deploy(MAINS);
+			break;
+		}
+		case MSG_TYPE_REQ_WAKEUP: {
+			printf("Received REQ_WAKEUP\n");
+			resp_msg_cls = MSG_TYPE_POST_PINGPONG;
+			resp_msg_payload.single_byte.single_byte_payload = 0x12;
+			resp_msg_payload_len = 1;
+
+			vTaskDelay(5000 / portTICK_PERIOD_MS); // forgive me for i have sinned.
+
+			//WAKEUP LOGIC GOES HERE @worldwalker2000
+			bool value = atomic_load(&thread_safe_should_wakeup);
+			atomic_store(&thread_safe_should_wakeup, !value);
+
+			break;
+		}
+		default: {
+			#ifdef LORA_DEBUG
+			printf("Unknown MSG_TYPE: 0x%X\n", request_msg_type);
+			#endif
+			break;
+		}
+	}
+}
 
 goober_payload_t create_telemetry_payload(int32_t latitude, int32_t longitude, float altitude_agl, float vertical_velocity, float x_acc, float eul_x, float eul_y, float eul_z, float gyr_x, uint8_t sats, uint8_t flight_state) {
 	goober_payload_t payload;
@@ -99,6 +241,14 @@ goober_payload_t create_telemetry_payload(int32_t latitude, int32_t longitude, f
 	payload.telemetry.battery_voltage = (uint32_t)(psu_read_battery_voltage() * 100000);
 
   return payload;
+}
+
+bool is_tx_lock() {
+	return atomic_load(&thread_safe_txlock);
+}
+
+bool should_wake_up() {
+	return atomic_load(&thread_safe_should_wakeup);
 }
 
 //this is just here for now :p
