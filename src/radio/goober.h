036f29421500d630de3fd7d81b4b4d3f4673e727
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-10-16 16:17:38 -0700

    partial cleanup, updated GPS struct

--- src/radio/goober.h
+++ src/radio/goober.h
@@ -95,6 +95,14 @@ bool is_tx_lock();
 
 bool should_wake_up();
 
+void turn_off_cameras(void);
+void turn_off_fan(void);
+void fake_tx_lock(void);
+void turn_on_cameras(void);
+void turn_on_fan(void);
+void flash_erase_jingle(void);
+            
+
 void activate_txlock();
 
 #endif
