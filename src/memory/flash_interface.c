d8ab6588403acf220e9ade5a0f1327f79ca00804
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-10-17 08:04:43 -0700

    We goooo faaaaastttt

--- src/memory/flash_interface.c
+++ src/memory/flash_interface.c
@@ -270,14 +270,14 @@ void flash_write_packet(flash_packet *packet) {
 
 void flash_queue_packet(flash_packet *packet) {
     packet->n = n++;
-    if (xQueueSendToBack(flash_packet_queue, packet, pdMS_TO_TICKS(MUTEX_TIMEOUT)) != pdTRUE) {
+    if (xQueueSendToBack(flash_packet_queue, packet, 0) != pdTRUE) {
         // vTaskDelay(pdMS_TO_TICKS(1)); 
         // printf("QUEUE OVER RUN, FLASH PACKET LOST\n");
 
         flash_packet oldItem;
-        xQueueReceive(flash_packet_queue, &oldItem, pdMS_TO_TICKS(MUTEX_TIMEOUT));
+        xQueueReceive(flash_packet_queue, &oldItem, 0);
         
-        if (xQueueSendToBack(flash_packet_queue, packet, pdMS_TO_TICKS(MUTEX_TIMEOUT)) != pdTRUE) {
+        if (xQueueSendToBack(flash_packet_queue, packet, 0) != pdTRUE) {
             // vTaskDelay(pdMS_TO_TICKS(1)); 
             printf("QUEUE OVER RUN, FLASH PACKET LOST\n");
         }
--- src/sensor/sensor_manager.c
+++ src/sensor/sensor_manager.c
@@ -231,15 +231,22 @@ void bno_flight_init(){
     vTaskDelay(10 / portTICK_PERIOD_MS);
     bno_configure_acc(NORMAL, ACC_C_H1000, ACC_C_RANGE_16G);  //Normal power, 1kHz ODR, 16G range
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    bno_set_acc_int(true, true, true, true, true, true, 2); // HG on X/Y/Z
+    bno_set_acc_int(false, false, false, false, false, false, 2); // HG on X/Y/Z
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    bno_setinterruptenable(false, true, false, false, false, false, false, false);
+    bno_setinterruptenable(false, false, false, false, false, false, false, false);
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    bno_setinterruptmask(false, true, false, false, false, false, false, false);
+    bno_setinterruptmask(false, false, false, false, false, false, false, false);
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    bno_set_acc_hgtresh(187);
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-    bno_set_acc_hgduration(10);
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+    // bno_set_acc_int(true, true, true, true, true, true, 2); // HG on X/Y/Z
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+    // bno_setinterruptenable(false, true, false, false, false, false, false, false);
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+    // bno_setinterruptmask(false, true, false, false, false, false, false, false);
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+    // bno_set_acc_hgtresh(187);
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+    // bno_set_acc_hgduration(10);
     vTaskDelay(10 / portTICK_PERIOD_MS);
     bno_setoprmode(AMG);
 
@@ -264,19 +271,19 @@ void lis331_flight_init(){
     h3lis331dl_set_endian(H3LIS331DL_BIG_ENDIAN);
 
     //high-side int detection
-    uint8_t enables_mask = (1 << 1) | (1 << 3); //XHIE and YHIE
-    h3lis331dl_set_int_cfg(H3LIS331DL_INT1, enables_mask, false);//OR mode
+    // uint8_t enables_mask = (1 << 1) | (1 << 3); //XHIE and YHIE
+    // h3lis331dl_set_int_cfg(H3LIS331DL_INT1, enables_mask, false);//OR mode
 
-    //Threshold: 2.828g (Net 4G with gravity -> 3g power acceleration)
-    h3lis331dl_set_int_threshold(H3LIS331DL_INT1, 2.828);
+    // //Threshold: 2.828g (Net 4G with gravity -> 3g power acceleration)
+    // h3lis331dl_set_int_threshold(H3LIS331DL_INT1, 2.828);
 
-    // Duration: 5 → 5ms @ 1000Hz
-    h3lis331dl_set_int_duration(H3LIS331DL_INT1, 5);
+    // // Duration: 5 → 5ms @ 1000Hz
+    // h3lis331dl_set_int_duration(H3LIS331DL_INT1, 5);
 
-    // Optional cleanup
-    h3lis331dl_set_int_level(H3LIS331DL_INT_ACTIVE_LOW);
-    h3lis331dl_set_int_pin_mode(H3LIS331DL_INT_PUSH_PULL);
-    h3lis331dl_set_int1_latch(true);
+    // // Optional cleanup
+    // h3lis331dl_set_int_level(H3LIS331DL_INT_ACTIVE_LOW);
+    // h3lis331dl_set_int_pin_mode(H3LIS331DL_INT_PUSH_PULL);
+    // h3lis331dl_set_int1_latch(true);
 }
 
 
