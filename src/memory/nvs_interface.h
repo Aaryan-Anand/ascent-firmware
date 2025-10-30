4995a18df3f6e1367f00edf723c44e2b20de8ca2
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-10-29 19:58:40 -0700

    Bug fixes

--- src/memory/nvs_interface.h
+++ src/memory/nvs_interface.h
@@ -8,6 +8,6 @@ void nvs_interface_init(void);
 
 nvs_handle_t nvs_interface_get_handle(void);
 
-void nvs_retreive_matrices(float *acc_correction_matrix, float *gyr_correction_matrix, float *mag_correction_matrix, float *high_g_correction_matrix, float *acc_bias_vector, float *gyr_bias_vector, float *mag_bias_vector, float *high_g_bias_vector);
-void nvs_retreive_uuid(uint8_t *uuid[16]);
+void nvs_retreive_matrices(float (*acc_correction_matrix)[3], float (*gyr_correction_matrix)[3], float (*mag_correction_matrix)[3], float (*high_g_correction_matrix)[3], float acc_bias_vector[3], float gyr_bias_vector[3], float mag_bias_vector[3], float high_g_bias_vector[3]);
+void nvs_retreive_uuid(uint8_t *uuid);
 #endif
