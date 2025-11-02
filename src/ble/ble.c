ba9792086291c7113ba51a9ec0c9adcc5bd5ae06
Abdul Zia <abdulzia@asu.edu>
2025-11-01 23:31:16 -0700

    stop the NVS flash from being erased by BLE driver -- BLE must be initialized after NVS.

--- src/ble/ble.c
+++ src/ble/ble.c
@@ -243,14 +243,7 @@ void
 ble_init(void)
 {
     int rc;
-
-    /* Initialize NVS — it is used to store PHY calibration data */
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    esp_err_t ret;
 
     ret = nimble_port_init();
     if (ret != ESP_OK) {
