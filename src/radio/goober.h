d8b82aa7d91a1e6d3bf146185f29c0231aa2ab16
Abdul Zia <abdulzia@asu.edu>
2025-11-03 09:57:15 -0700

    nvs_dump message

--- src/radio/goober.h
+++ src/radio/goober.h
@@ -38,6 +38,7 @@ typedef enum {
     MSG_TYPE_REQ_POP_APOGEE = 0x69,
     MSG_TYPE_REQ_POP_MAINS = 0x6A,
     MSG_TYPE_REQ_WAKEUP = 0x6B,
+    MSG_TYPE_NVS_DUMP = 0x72,
     MSG_TYPE_NVS_EDIT_FLIGHT = 0x73,
     MSG_TYPE_NVS_EDIT_LORA = 0x74
 } goober_msg_type_t;
@@ -66,6 +67,11 @@ typedef struct {
     uint8_t single_byte_payload; // 1 byte payload
 } goober_post_single_byte_payload_t;
 
+typedef struct {
+    flight_config_t flight_config;
+    lora_config_t lora_config;
+} nvs_dump_t;
+
 // Union of payloads
 
 typedef union goober_payload {
@@ -74,6 +80,7 @@ typedef union goober_payload {
     goober_post_single_byte_payload_t single_byte;
     flight_config_t flight_config;
     lora_config_t lora_config;
+    nvs_dump_t nvs_dump;
     uint8_t raw[ sizeof(goober_post_telemetry_payload_t) ];
 } goober_payload_t;
 
